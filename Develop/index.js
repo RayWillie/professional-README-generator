3a9c96b41896a1cb0ed4dcf32219543cf064821f
mac Ray <rztechdude@yahoo.com>
2022-03-12 16:13:52 -0500

    updated markdown.jd

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquire = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquire.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
+
+        },
+        {
+            type: 'input',
+            name: 'descriptionInput',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
+ 
+        }
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
